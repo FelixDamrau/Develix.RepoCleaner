746e5be7ab6bfc4a30f0c1a276c00a8027a64b15
Felix Damrau <mail@felixdamrau.de>
2022-10-10 22:11:58 +0200

    Add base class for customoverview table rows

--- src/RepoCleaner/ConsoleComponents/OverviewTableRowAuthor.cs
+++ src/RepoCleaner/ConsoleComponents/OverviewTableRowAuthor.cs
@@ -1,37 +1,9 @@
-﻿using System.Reflection;
-using Spectre.Console;
+﻿namespace Develix.RepoCleaner.ConsoleComponents;
 
-namespace Develix.RepoCleaner.ConsoleComponents;
-
-internal class OverviewTableRowAuthor : OverviewTableRowBase
+internal class OverviewTableRowAuthor : OverviewTableRowCustomBase
 {
-    private readonly string author;
-    private readonly List<PropertyInfo> columnPropertyInfos;
-    private readonly OverviewTableRowBase parentRow;
-
     public OverviewTableRowAuthor(OverviewTableRowBase parentRow, string author)
+        : base(parentRow, ":pencil:", author)
     {
-        this.author = author;
-        columnPropertyInfos = GetStringPropertyInfos(parentRow.GetType()).Select(x => x.Property).ToList();
-        this.parentRow = parentRow;
-    }
-
-    public override IEnumerable<string> GetColumns() => parentRow.GetColumns();
-
-    public override IEnumerable<Markup> GetRowData()
-    {
-        var workItemTypeIndex = GetColumIndex(nameof(OverviewTableRow.WorkItemTypeString));
-        var workItemTitleIndex = GetColumIndex(nameof(OverviewTableRow.Title));
-
-        var authorColumns = Enumerable.Repeat(new Markup(string.Empty), columnPropertyInfos.Count).ToArray();
-        authorColumns[workItemTypeIndex] = new Markup(":pencil:");
-        authorColumns[workItemTitleIndex] = new Markup($"[grey30]{author}[/]");
-        return authorColumns;
-    }
-
-    private int GetColumIndex(string columnpropertyName)
-    {
-        var columnPropertyInfo = columnPropertyInfos.Single(pi => pi.Name == columnpropertyName);
-        return columnPropertyInfos.IndexOf(columnPropertyInfo);
     }
 }
---
+++ src/RepoCleaner/ConsoleComponents/OverviewTableRowCustomBase.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using Spectre.Console;
+
+namespace Develix.RepoCleaner.ConsoleComponents;
+
+internal abstract class OverviewTableRowCustomBase : OverviewTableRowBase
+{
+    protected readonly PropertyInfo[] columnPropertyInfos;
+    protected readonly OverviewTableRowBase parentRow;
+
+    protected string Icon { get; }
+    protected string Data { get; }
+
+    public OverviewTableRowCustomBase(OverviewTableRowBase parentRow, string icon, string data)
+    {
+        columnPropertyInfos = GetStringPropertyInfos(parentRow.GetType()).Select(x => x.Property).ToArray();
+        this.parentRow = parentRow;
+        Icon = icon;
+        Data = data;
+    }
+
+    public override IEnumerable<string> GetColumns() => parentRow.GetColumns();
+
+    public override IEnumerable<Markup> GetRowData()
+    {
+        var workItemTypeIndex = GetColumIndex(nameof(OverviewTableRow.WorkItemTypeString));
+        var titleIndex = GetColumIndex(nameof(OverviewTableRow.Title));
+
+        var columns = Enumerable.Repeat(new Markup(string.Empty), columnPropertyInfos.Length).ToArray();
+        columns[workItemTypeIndex] = new Markup(Icon);
+        columns[titleIndex] = new Markup($"[grey30]{Data}[/]");
+        return columns;
+    }
+
+    protected int GetColumIndex(string columnpropertyName)
+    {
+        var columnPropertyInfo = columnPropertyInfos.Single(pi => pi.Name == columnpropertyName);
+        return Array.IndexOf(columnPropertyInfos, columnPropertyInfo);
+    }
+
+}
