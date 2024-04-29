4c8d439a6fb8a90d7ccf8d0af1e6dc11de880bba
Felix Damrau <mail@felixdamrau.de>
2024-04-29 21:24:22 +0200

    Add status spinner

--- src/RepoCleaner/ConsoleComponents/Cli/RepoCleanerCommand.cs
+++ src/RepoCleaner/ConsoleComponents/Cli/RepoCleanerCommand.cs
@@ -26,36 +26,49 @@ internal class RepoCleanerCommand : AsyncCommand<RepoCleanerSettings>
 
     public override async Task<int> ExecuteAsync(CommandContext context, RepoCleanerSettings settings)
     {
-        var loginResult = await AzdoClient.Login(reposService, workItemService, settings, appSettings);
-        if (!Validate(loginResult))
-            return 1;
+        var status = await AnsiConsole
+            .Status()
+            .Spinner(Spinner.Known.Dots)
+            .StartAsync("Starting RepoCleaner", async ctx =>
+        {
+            ctx.Status("Logging in");
+            var loginResult = await AzdoClient.Login(reposService, workItemService, settings, appSettings);
+            if (!Validate(loginResult))
+                return 1;
 
-        var repositoryResult = RepositoryInfo.Get(settings, appSettings);
-        if (!Validate(repositoryResult))
-            return 2;
+            ctx.Status("Getting repository");
+            var repositoryResult = RepositoryInfo.Get(settings, appSettings);
+            if (!Validate(repositoryResult))
+                return 2;
 
-        var workItemsIds = RepositoryInfo.GetRelatedWorkItemIds(repositoryResult.Value);
-        var workItemsResult = await workItemService.GetWorkItems(workItemsIds, settings.IncludePullRequests);
-        if (!Validate(workItemsResult))
-            return 3;
+            ctx.Status("Getting work items");
+            var workItemsIds = RepositoryInfo.GetRelatedWorkItemIds(repositoryResult.Value);
+            var workItemsResult = await workItemService.GetWorkItems(workItemsIds, settings.IncludePullRequests);
+            if (!Validate(workItemsResult))
+                return 3;
 
-        var table = new OverviewTable(appSettings, settings);
-        var renderedTable = table.GetOverviewTable(workItemsResult.Value, repositoryResult.Value);
-        AnsiConsole.Write(renderedTable);
+            ctx.Status("Rendering output");
+            var table = new OverviewTable(appSettings, settings);
+            var renderedTable = table.GetOverviewTable(workItemsResult.Value, repositoryResult.Value);
+            AnsiConsole.Write(renderedTable);
 
-        if (settings.Delete)
-            ExecuteDelete(repositoryResult.Value, workItemsResult.Value);
+            ctx.Status("Waiting for branches to delete");
+            if (settings.Delete)
+                ExecuteDelete(repositoryResult.Value, workItemsResult.Value);
 
-        return 0;
+            return 0;
+        });
+        return status;
     }
 
     private static void ExecuteDelete(Repository repository, IEnumerable<WorkItem> workItems)
     {
-        var x = Delete.Prompt(repository, workItems);
-        Delete.Execute(repository, x);
+        var branchesToDelete = Delete.Prompt(repository, workItems);
+        Delete.Execute(repository, branchesToDelete);
     }
 
-    private static bool Validate(IResult result) { 
+    private static bool Validate(IResult result)
+    {
         if (!result.Valid)
         {
             var message = $"""
